75c4fe699c06195499ad651a66852156020efb61
your_github_DannyGarciaDEV <dannygarciadev@gmail.com>
2023-05-23 23:47:43 -0400

    Moved create message logic to event.js

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -539,38 +539,4 @@ function copyWebsiteLink() {
   // You can perform any other action here
 }
 
-var msgBtn = document.getElementsByClassName("msgBtn");
-
-// TODO research on getById() for the button and any other data inputs
-Array.from(msgBtn).forEach(function (element) {
-  element.addEventListener('click', function (e) {
-    e.preventDefault();
-    let content = this.parentNode.childNodes[1].value
-    // NEVER TRUST THE CLIENT
-    // let summary = this.parentNode.parentNode.parentNode.childNodes[41].childNodes[1].innerHTML
-    // let user = this.parentNode.parentNode.parentNode.childNodes[41].childNodes[3].innerHTML
-
-    // TODO POST message
-    // TODO update to only include message attributes
-    // TODO check if we need the event ID
-    const eventIdRoute = window.location.pathname;
-    fetch(`${eventIdRoute}/message`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        'content': content
-      })
-    })
-      .then(response => {
-        if (response.ok) return response.json()
-      })
-      .then(data => {
-        window.location.reload(true);
-        // TODO update the html with the new message
-
-      })
-  })
-})
 
