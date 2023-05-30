7aedf0ad6b8cb53a9c463c4c96b570cede70c086
your_github_DannyGarciaDEV <dannygarciadev@gmail.com>
2023-05-30 14:08:58 -0400

    removing db

--- config/database.js
+++
@@ -1,6 +0,0 @@
-// config/database.js
-module.exports = {
-
-    'url' : 'mongodb+srv://dannygarciadev:rUfN6tMzFujP3x@cluster0.nbqmops.mongodb.net/kpop?retryWrites=true&w=majority', 
-    'dbName': 'demoday'
-};
